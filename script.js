3106eec36ceabdd0530c0a074681665874adbad7
pedro <pedromportilho@gmail.com>
2022-09-14 17:45:42 -0300

    Alteração do botão do header

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// const email = document.querySelector('#email');
-// const password = document.querySelector('#password');
-// const entrar = document.querySelector('#entrar')
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const entrar = document.getElementById('entrar');
 
-// entrar.addEventListener('click', function () {
-//   if (email.value === 'tryber@teste.com' && password.value === '123456') {
-//     alert('bem vindo!');
-//   };
-// });
+function validar() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
 
+entrar.addEventListener('click', validar);
