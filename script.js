7736d3c1aa955025eb4daa2236b07d01c99cbc29
David Lira <davexlira@hotmail.com>
2022-09-14 16:56:51 -0300

    adiciona inputs de login e footer

---
+++ script.js
@@ -0,0 +1,9 @@
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+// const entrar = document.querySelector('#entrar')
+
+// entrar.addEventListener('submit', function () {
+//   if(email.value === 'tryber@teste.com' && password.value === '123456') {
+//     alert('bem vindo!');
+//   };
+// });
