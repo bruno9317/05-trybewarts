68a0bcf7507a94108c1b8408baddf0dceed51af1
pedro <pedromportilho@gmail.com>
2022-09-14 17:08:19 -0300

    Requisito 3 corrigido

--- script.js
+++ script.js
@@ -7,3 +7,17 @@
 //     alert('bem vindo!');
 //   };
 // });
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const entrar = document.getElementById('entrar');
+
+function validar() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+entrar.addEventListener('click', validar);
